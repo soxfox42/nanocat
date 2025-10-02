cdc81a36cdf7becbfeb236e3210a1c9ad9c0c12e
Finn Coffey <finn.coffey@gmail.com>
2025-10-02 20:26:20 +1000

    sixel trimming + dark sixels

--- sixel.py
+++ sixel.py
@@ -16,6 +16,44 @@ class Sixel:
                     pixel = (value >> y) & 1
                     self.pixels.append(pixel)
 
+        self.height = len(self.pixels) // self.width
+
+        self.trim()
+
+    def trim(self):
+        top = self.hborder(0, 1)
+        bottom = self.hborder(self.height - 1, -1)
+        left = self.vborder(0, 1)
+        right = self.vborder(self.width - 1, -1)
+
+        print(self.width, self.height)
+        result = []
+        for y in range(top, bottom + 1):
+            start = y * self.width + left
+            end = y * self.width + right + 1
+            result += self.pixels[start:end]
+        self.pixels = result
+        self.width = right - left + 1
+        self.height = bottom - top + 1
+        print(self.width, self.height)
+
+    def hborder(self, start, step):
+        y = start
+        while 0 <= y < self.height:
+            if any(
+                pixel == 1
+                for pixel in self.pixels[y * self.width : (y + 1) * self.width]
+            ):
+                return y
+            y += step
+
+    def vborder(self, start, step):
+        x = start
+        while 0 <= x < self.width:
+            if any(pixel == 1 for pixel in self.pixels[x :: self.width]):
+                return x
+            x += step
+
     def dump(self):
         chars = " ▄▀█"
         i = 0
@@ -32,11 +70,13 @@ class Sixel:
             i += self.width * 2
 
     def to_wx_image(self):
-        width = self.width
-        height = len(self.pixels) // width
+        dark = wx.SystemSettings.GetAppearance().IsDark()
+
         rgb_data = []
         alpha_data = []
         for pixel in self.pixels:
-            rgb_data += [(1 - pixel) * 255] * 3
+            if not dark:
+                pixel = 1 - pixel
+            rgb_data += [pixel * 255] * 3
             alpha_data.append(pixel * 255)
-        return wx.Image(width, height, bytes(rgb_data), bytes(alpha_data))
+        return wx.Image(self.width, self.height, bytes(rgb_data), bytes(alpha_data))
